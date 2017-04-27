40786b3be38dd7bc0eccb66a98a85742fe7e3846
Blin Mark <anwardo13@gmail.com>
2017-04-27 19:44:32 +0300

    X

--- blog/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Order
-
-
-class OrderCreateForm(forms.ModelForm):
-    class Meta:
-        model = Order
-        fields = ['name', 'sername', 'phone', 'email', 'product_format',
-                    'product_size', 'product_orientation', 'product_kreplenie', 'product']
